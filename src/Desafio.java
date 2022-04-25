2e1129a7968dda1faceb6ae85c3ba4fe6d87f912
Sérgio Augusto David Jr <sergio.adjr@gmail.com>
2022-04-25 16:07:20 -0400

    atualizacao3.1

---
+++ src/Desafio.java
@@ -0,0 +1,13 @@
+public class Desafio {
+public static void main(String[] args) {
+    int anterior = 0;
+    int atual =1;
+    while (atual<100) {
+        System.out.println(atual);
+        atual = anterior+atual;
+        anterior=atual-anterior;
+    }
+System.out.println(atual);
+}
+    
+}
---
+++ src/Exercicio29.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Exercicio29 {
+    public static void main(String[] args) {
+    Scanner leitor = new Scanner(System.in);
+    System.out.println("Informe um número inteiro");
+    int numero1 = leitor.nextInt();
+    
+
+
+
+    }
+}
